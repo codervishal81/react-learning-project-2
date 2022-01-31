7e07c8771ec449ae237d09e0cd0e0f2090085636
vishal shah <vishal@imorphr.com>
2022-01-31 18:31:26 +0530

    Added Fragments and Portals
    Added Fragments and Portals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, Fragment } from 'react';
 import AddUser from './components/users/AddUser';
 import UsersList from './components/users/UsersList';
 
@@ -6,18 +6,23 @@ function App() {
   const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (userName, userAge) => {
-    setUsersList((prevUsersList) => [...prevUsersList, {id: Math.random().toString(),name: userName, age: userAge}]);
+    setUsersList((prevUsersList) => [...prevUsersList, { id: Math.random().toString(), name: userName, age: userAge }]);
   };
 
+  // To avoid div soup problem we can use either <></> as root wrapping element. Or we can use <React.Fragment> element as wrapping element.
+
+  /* return (
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
+  ); */
+
   return (
-    <div>
-      <div>
-        <AddUser onAddUser={addUserHandler}/>
-      </div>
-      <div>
-        <UsersList users={usersList}/>
-      </div>
-    </div>
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </Fragment>
   );
 }
 
--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "../ui/Card";
 import classes from "./AddUser.module.css"
 import Button from "../ui/Button";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import ErrorModal from "../ui/ErrorModal";
 
 const AddUser = (props) => {
@@ -40,7 +40,7 @@ const AddUser = (props) => {
     };
     
     return (
-        <div>
+        <Fragment>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
@@ -53,7 +53,7 @@ const AddUser = (props) => {
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Fragment>
     );
 };
 
